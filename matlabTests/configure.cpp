ee91a9dfef808ebbd1e192455995fcad691e9863
nlacock@udel.edu <nlacock@udel.edu@805e5a2e-fec6-8ac6-4df9-c35017460c5f>
2011-06-13 19:02:05 +0000

    Integrated config file setup with mex files, made a Makefile

---
+++ matlabTests/configure.cpp
@@ -0,0 +1,74 @@
+#include <libconfig.h++>
+#include <iostream>
+#include "Environment.h"
+#include "Obstacle.h"
+#include "MPN2D.h"
+
+#include "configure.h"
+
+#define DEFAULTFILE "default.cfg"
+
+using namespace libconfig;
+using namespace std;
+
+void configure(char file[],Environment * & e,MPNParams * & mp){
+
+  Config c;
+  c.readFile(file);
+
+  double radius = c.lookup("environment.radius");
+  double pot_param = c.lookup("environment.potential_parameter");
+  double goal[2] = {c.lookup("environment.destination")[0],
+		 c.lookup("environment.destination")[1]};
+
+  e  = new Environment(goal,pot_param,radius);
+
+  if(c.exists("environment.obstacles")){
+    double rad,pos[2];
+    Setting & obs = c.lookup("environment.obstacles");
+    for(int i(0); i<obs.getLength(); i++){
+      rad = obs[i]["radius"];
+      pos[0] = obs[i]["position"][0];
+      pos[1] = obs[i]["position"][1];
+      e->obstacles.push_back(Obstacle(pos,rad));
+    }
+  }
+
+  //Initialize mandatory parameters first
+  mp = new MPNParams;
+  mp->nLegendrePolys = c.lookup("mpn_parameters.num_legendre_polys");
+  mp->confidence = c.lookup("mpn_parameters.confidence");
+  mp->level = c.lookup("mpn_parameters.level");
+  mp->predictionHorizon = c.lookup("mpn_parameters.prediction_horizon");
+  mp->controlHorizon = c.lookup("mpn_parameters.control_horizon");
+  mp->currentTime = c.lookup("mpn_parameters.start_time");
+
+  if(c.exists("mpn_parameters.cost_weights")){
+    Setting & cw = c.lookup("mpn_parameters.cost_weights");
+    for(int i(0); i<3; i++){
+      mp->costWeights[i] = cw[i];
+    }
+  }
+  else{
+    for(int i(0); i<3; i++)
+      mp->costWeights[i] = 0;
+  }
+
+  mp->controlParameters = new double[mp->nLegendrePolys];
+  if(c.exists("mpn_parameters.control")){
+    Setting & cp = c.lookup("mpn_parameters.control");
+    for(int i(0); i<mp->nLegendrePolys; i++){
+      mp->controlParameters[i] = cp[i];
+    }
+  }
+  else{
+    for(int i(0); i<mp->nLegendrePolys; i++)
+      mp->controlParameters[i] = 0;
+  }
+
+}
+
+void configure(Environment * & e,MPNParams * & mp){
+  char filename[] = DEFAULTFILE;
+  configure(filename,e,mp);
+}
--- matlabTests/samplePath.cpp
+++ matlabTests/samplePath.cpp
@@ -1,7 +1,7 @@
 #include "mex.h"
-#include "setupEnv.h"
 #include "Environment.h"
 #include "MPN2D.h"
+#include "configure.h"
 
 #define DEFAULT_N_LEGENDRE 5
 
@@ -9,22 +9,17 @@
   Go-between from matlab to MPN2D::samplePath for visualization
   To customize the environment, see setupEnv.cpp
 
-  Input arguments: [x,y] start, [x,y] goal, control parameters (vector same size
-  as n_legendre), current time, prediction horizon, radius, precision(dt), steps
+  Input arguments: [x,y] start, precision(dt), steps, (optional filename)
   Output arguments: row vector of x values, row vector of y values (optional 
   row vector of dx values, row vector of dy values)
 
   Example usage in matlab:
-  >>> [x,y] = samplePath([.6,.4],[.5,.5],zeros(1,5),0.,1.,1.,.01,100);
-  >>> plot(x,y)
-  Example 2:
-  >>> [x,y,dx,dy] = samplePath([.6,.4],[.5,.5],zeros(1,5),0.,1.,1.,.01,100);
-  >>> plot(dx,dy)
+
 
 */
 void mexFunction(int nlhs, mxArray *plhs[ ],int nrhs, const mxArray *prhs[ ]) {
   //Check for correct function syntax
-  if(nrhs != 8)
+  if(!(nrhs == 3 || nrhs == 4) )
     mexErrMsgTxt("Incorrect number of input arguments");
   if(!(nlhs == 2 || nlhs == 4))
     mexErrMsgTxt("Incorrect number of output arguments");
@@ -34,33 +29,24 @@ void mexFunction(int nlhs, mxArray *plhs[ ],int nrhs, const mxArray *prhs[ ]) {
     mexErrMsgTxt("Start position must be in [x,y] form");
   double start[2] = {mxGetPr(prhs[0])[0],mxGetPr(prhs[0])[1]};
 
-  //Get goal position and check for correct dimensions
-  if(mxGetM(prhs[1]) != 1 || mxGetN(prhs[1]) != 2)
-    mexErrMsgTxt("Goal position must be in [x,y] form");
-  double goal[2] = {mxGetPr(prhs[1])[0],mxGetPr(prhs[1])[1]};
-
-  //Get control parameters and check for correct dimensions
-  if(mxGetM(prhs[2]) != 1 || mxGetN(prhs[2]) != DEFAULT_N_LEGENDRE)
-    mexErrMsgTxt("Number of control parameters must match number of legendre polynomials, in row vector form");
-  double controlParams[DEFAULT_N_LEGENDRE];
-  for(int i(0); i<DEFAULT_N_LEGENDRE; i++){
-    controlParams[i] = mxGetPr(prhs[2])[i];
-  }
- 
-  MPNParams params;
-  params.nLegendrePolys = DEFAULT_N_LEGENDRE;
-  params.controlParameters = controlParams;
-
-  //Get current time, prediction horizon, radius, precision(dt), steps
-  params.currentTime = *mxGetPr(prhs[3]);
-  params.predictionHorizon = *mxGetPr(prhs[4]);
-  double radius = *mxGetPr(prhs[5]);
-  double dt = *mxGetPr(prhs[6]);
-  unsigned int steps = static_cast<int>(*mxGetPr(prhs[7]));
+  //Get precision,steps
+  double dt = *mxGetPr(prhs[1]);
+  unsigned int steps = static_cast<int>(*mxGetPr(prhs[2]));
 
   //Initialize environment
-  Environment e = *setupEnv(goal,radius);
+  Environment * e;
+  MPNParams * mp;
+  if(nrhs == 4){
+    char filename[256];
+    mxGetString(prhs[3],filename,mxGetN(prhs[3])+1);
+    mexPrintf(filename);
+    configure(filename,e,mp);
+  }
+  else{
+    configure(e,mp);
+  }
 
+  
   //Allocate space for the answer
   plhs[0] = mxCreateDoubleMatrix(1,steps,mxREAL);
   plhs[1] = mxCreateDoubleMatrix(1,steps,mxREAL);
@@ -72,7 +58,7 @@ void mexFunction(int nlhs, mxArray *plhs[ ],int nrhs, const mxArray *prhs[ ]) {
   //Calculate a sample path
   double ** path = allocatePoints(steps);
   double ** controlPath = allocatePoints(steps);
-  samplePath(e,params,controlPath,path,start,dt,steps);
+  samplePath(*e,*mp,controlPath,path,start,dt,steps);
 
   //Copy the answer into the matlab vectors for output
   for(int i(0); i<steps; i++){
