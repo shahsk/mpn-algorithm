7d3d9a25c641d5ace146e37b50adcf68990b67db
nick.lacock@gmail.com <nick.lacock@gmail.com@805e5a2e-fec6-8ac6-4df9-c35017460c5f>
2011-12-03 23:35:57 +0000

    Vectorized gamma and more trial data

--- branches/optimize/gamma.cpp
+++ branches/optimize/gamma.cpp
@@ -2,6 +2,10 @@
 #include "gamma.h"
 #include <math.h>
 
+#ifdef NEON
+#include <arm_neon.h>
+#endif
+
 #ifndef DIM2
 
 //Returns the n-dimensional distance SQUARED between 2 points
@@ -26,10 +30,20 @@ mpn_float gamma(mpn_float*v,unsigned int deprecated){
   return v[0]*v[0] + v[1]*v[1];
 }
 
+#if (defined SINGLE_PRECISION && defined ARM)
+mpn_float gamma(mpn_float*v1,mpn_float*v2,unsigned int deprecated){
+  float32x2_t a = vld1_f32(v1);
+  float32x2_t b = vld1_f32(v1);
+  a = vsub_f32(a,b);
+  a = vmul_f32(a,a);
+  return vget_lane_f32(a,0)+vget_lane_f32(a,1);
+}
+#else
 mpn_float gamma(mpn_float*v1,mpn_float*v2,unsigned int deprecated){
   mpn_float a = (v1[0]-v2[0]);
   mpn_float b = (v1[1]-v2[1]);
   return a*a + b*b;
 }
+#endif
 
 #endif
